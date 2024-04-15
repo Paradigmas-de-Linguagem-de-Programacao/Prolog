7326552b480217a55c8eabc053e253f153ad0ca8
Gabriel-Emannuel <gabriel.emannuel.gama.andrade@gmail.com>
2024-04-15 20:41:44 -0300

    Atualizando main

--- app/main.pl
+++ app/main.pl
@@ -1,11 +1,12 @@
 /* essa estrutura de diretórios visa facilitar as consultas de prolog, para isso,
 tanto Tetris quanto FMCC devem no fliperama a partir daqui. */
 
-:- consult('Fmcc/fmcc.pl').
-:- consult('Tetris/tetris.pl').
+:- consult('../src/fmcc/fmcc.pl').
+:- consult('../src/tetris/tetris.pl').
+:- consult('../src/fliperama/fliperama.pl').
 
 main:-
     mock_fmcc,
-    nl,
     mock_tetris,
+    mock_fliperama,
     halt.
