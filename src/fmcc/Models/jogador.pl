46b87d732f1cb44f90e44c44e6745c918e7ef589
victor-vili-luna <victor.vili.xavier.luna@ccc.ufcg.edu.br>
2024-04-23 21:23:29 -0300

    prototipo finalizado

--- src/fmcc/Models/jogador.pl
+++ src/fmcc/Models/jogador.pl
@@ -1,18 +1,89 @@
-:- dynamyc jogador/7
-
-jogador(
-    nome(Nome),
-    vida(Vida),
-    gold(Gold),
-    ataque(Ataque),
-    defesa(Defesa),
-    equipamentos(Equipamentos),
-    pocoes(Pocoes)
-).
+:- dynamic jogador/5.
+:- dynamic jogador_combate/5.
 
 jogador_inicial :- 
-    asserta(jogador(nome("Heanes"), vida(100), gold(0), ataque(10), defesa(5), equipamentos([]), pocoes([]))).
+    asserta(jogador("Heanes", 0, [], [], 0)).
 
-atualiza_jogador(Nome, Vida, Gold, Ataque, Defesa, Equipamentos, Pocoes) :-
-    retract(jogador(_,_,_,_,_,_,_)) % exclui a ultima representacao de jogador
-    asserta(jogador(nome(Nome), vida(Vida), gold(Gold), ataque(Ataque), defesa(Defesa), equipamentos(Equipamentos), pocoes(Pocoes))). % adiciona uma nova representacao de jogador
+jogador_combate_inicial :- 
+    asserta(jogador_combate(100, 10, 5, [], 0)).
+
+get_gold(Dinheiro) :- 
+    jogador(_,Dinheiro,_,_,_).
+
+set_gold(Gold_ganho) :-
+    retract(jogador(Nome_antigo, Gold_antigo, Equipamentos_antigo, Pocoes_antiga, Progresso)),
+    Gold_atual is Gold_ganho + Gold_antigo,
+    asserta(jogador(Nome_antigo, Gold_atual, Equipamentos_antigo, Pocoes_antiga, Progresso)).
+
+get_equipamentos(Equips) :-
+    jogador(_,_, Equips,_,_).
+
+set_equipamentos(Equipamento_novo):-
+    retract(jogador(Nome_antigo, Gold_antigo, Equipamentos_antigo, Pocoes_antiga, Progresso)),
+    append([Equipamento_novo], Equipamentos_antigo, Resultado),
+    asserta(jogador(Nome_antigo, Gold_antigo, Resultado, Pocoes_antiga, Progresso)).
+
+get_pocoes(Poisson) :- 
+    jogador(_,_,_,Poisson,_).
+
+set_pocoes(Pocao_nova) :-
+    retract(jogador(Nome_antigo, Gold_antigo, Equipamentos_antigo, Pocoes_antiga, Progresso)),
+    append([Pocao_nova], Pocoes_antiga, Result),
+    asserta(jogador(Nome_antigo, Gold_antigo, Equipamentos_antigo, Result, Progresso)).
+
+get_progresso(Prog) :- 
+    jogador(_,_,_,_,Prog).
+
+set_progresso(Prog_novo) :-
+    retract(jogador(Nome_antigo, Gold_antigo, Equipamentos_antigo, Pocoes_antiga, _)),
+    asserta(jogador(Nome_antigo, Gold_antigo, Equipamentos_antigo, Pocoes_antiga, Prog_novo)).
+
+get_vida(Hp) :- 
+    jogador_combate(Hp,_,_,_,_).
+
+set_vida(Dano_sofrido) :-
+    retract(jogador(Hp_antigo, Ataque_antigo, Defesa_antiga, Potions_antiga, Tomadas_antiga)),
+    Hp_atual is Hp_antigo - Dano_sofrido,
+    asserta(jogador(Hp_atual, Ataque_antigo, Defesa_antiga, Potions_antiga, Tomadas_antiga)).
+
+get_ataque(Ataq) :- 
+    jogador_combate(_,Ataq,_,_,_).
+
+set_ataque(Ataq_ganho) :-
+    retract(jogador_combate(Hp_antigo, Ataque_antigo, Defesa_antiga, Potions_antiga, Tomadas_antiga)),
+    Ataq_atual is Ataque_antigo + Ataq_ganho,
+    asserta(jogador_combate(Hp_antigo, Ataq_atual, Defesa_antiga, Potions_antiga, Tomadas_antiga)).
+
+get_defesa(Def) :- jogador_combate(_,_,Def,_,_).
+
+set_defesa(Def_ganha) :-
+    retract(jogador_combate(Hp_antigo, Ataque_antigo, Defesa_antiga, Potions_antiga, Tomadas_antiga)),
+    Def_atual is Defesa_antiga + Def_ganha,
+    asserta(jogador_combate(Hp_antigo, Ataque_antigo, Def_atual, Potions_antiga, Tomadas_antiga)).
+
+get_pocoes_combate(Potions) :- 
+    jogador_combate(_,_,_,Potions,_).
+
+get_pocoes_tomadas(Tomadas) :- 
+    jogador_combate(_,_,_,_,Tomadas).
+
+set_pocoes_tomadas :-
+    retract(jogador_combate(Hp_antigo, Ataque_antigo, Defesa_antiga, Potions_antiga, Tomadas_antiga)),
+    Tomadas_atual is Tomadas_antiga + 1,
+    asserta(jogador_combate(Hp_antigo, Ataque_antigo, Defesa_antiga, Potions_antiga, Tomadas_atual)).
+
+formata_jogador(Nome, Gold, Equipamentos, Pocoes, Progresso, String) :-
+    format(atom(String), " ~w | Gold: ~d \n Equipamentos: ~q \n Pocoes: ~q \n Progresso: ~d\n", [Nome, Gold, Equipamentos, Pocoes, Progresso]).
+
+formata_jogador_combate(Vida, Ataque, Defesa, Pocoes, Qtde_pocoes_tomadas, String) :-
+    format(atom(String), " Vida: ~d | Ataque: ~d | Defesa: ~d \n Pocoes: ~q \n Pocoes Consumidas: ~d\n", [Vida, Ataque, Defesa, Pocoes, Qtde_pocoes_tomadas]).
+
+exibe_jogador :- 
+    jogador(Nome, Gold, Equipamentos, Pocoes, Progresso),
+    formata_jogador(Nome, Gold, Equipamentos, Pocoes, Progresso, String_resultado),
+    write(String_resultado).
+
+exibe_jogador_combate :-
+    jogador_combate(Vida, Ataque, Defesa, Pocoes, Qtde_pocoes_tomadas),
+    formata_jogador_combate(Vida, Ataque, Defesa, Pocoes, Qtde_pocoes_tomadas, String_resultado),
+    write(String_resultado).
--- src/fmcc/Models/loja.pl
+++ src/fmcc/Models/loja.pl
@@ -1,63 +1,51 @@
 :- dynamic loja/3.
 
-loja(
-    espada(Espada),
-    armadura(Armadura),
-    pocao(Pocao)
-).
-
-espada(nome(Nome), preco(Preco), ataque(Ataque), defesa(Defesa), descricao(Descricao)).
-armadura(nome(Nome), preco(Preco), ataque(Ataque), defesa(Defesa), descricao(Descricao)).
-pocao(nome(Nome), preco(Preco),vida(Vida), ataque(Ataque), defesa(Defesa), quantidade(Quantidade)).
-
 setup_inicial :- 
 
     asserta(loja(
-        espada(nome("Espada de Pedra"), preco(30), ataque(30), defesa(0), descricao("So usa espada quem nao se garante.")),
-        armadura(nome("Armadura de Couro"), preco(30), ataque(0), defesa(30), descricao("Proteger nem protege muito, mas se tu ta achando ruim vai sem!!")),
-        pocao(nome("Café"), preco(20), vida(40), ataque(0), defesa(0), quantidade(1)))).
+        espada("Espada de Pedra", 30, 30, 0, "So usa espada quem nao se garante."),
+        armadura("Armadura de Couro", 30, 0, 30, "Proteger nem protege muito, mas se tu ta achando ruim vai sem."),
+        pocao("Café", 20, 40, 0, 0, 1))).
 
 atualiza_loja :-
 
     retract(loja(
-        espada(nome("Espada de Pedra"), preco(30), ataque(30), defesa(0), descricao("So usa espada quem nao se garante.")),
-        armadura(nome("Armadura de Couro"), preco(30), ataque(0), defesa(30), descricao("Proteger nem protege muito, mas se tu ta achando ruim vai sem!!")),
-        pocao(nome("Café"), preco(20), vida(40), ataque(0), defesa(0), quantidade(1)))),
+        espada("Espada de Pedra", 30, 30, 0, "So usa espada quem nao se garante."),
+        armadura("Armadura de Couro", 30, 0, 30, "Proteger nem protege muito, mas se tu ta achando ruim vai sem."),
+        pocao("Café", 20, 40, 0, 0, 1))),
     
     asserta(loja(
-        espada(nome("Espada de Ferro"), preco(100), ataque(60), defesa(0), descricao("Espada um pouco enferrujada, se nao matar na espadada mata no tetano")),
-        armadura(nome("Armadura de Ferro"), preco(100), ataque(0), defesa(60), descricao("Essa daqui até que protege contra as falácias da I.A.")),
-        pocao(nome("Red Bull"), preco(60), vida(30), ataque(20), defesa(20), quantidade(1)))).
+        espada("Espada de Ferro", 100, 60, 0, "Espada um pouco enferrujada, se nao matar na espadada mata no tetano."),
+        armadura("Armadura de Ferro", 100, 0, 60, "Essa daqui até que protege contra as falácias da I.A."),
+        pocao("Red Bull", 60, 30, 20, 20, 1))).
 
 loja_final :-
 
     retract(loja(
-        espada(nome("Espada de Ferro"), preco(100), ataque(60), defesa(0), descricao("Espada um pouco enferrujada, se nao matar na espadada mata no tetano")),
-        armadura(nome("Armadura de Ferro"), preco(100), ataque(0), defesa(60), descricao("Essa daqui até que protege contra as falácias da I.A.")),
-        pocao(nome("Red Bull"), preco(60), vida(30), ataque(20), defesa(20), quantidade(1)))),
+        espada("Espada de Ferro", 100, 60, 0, "Espada um pouco enferrujada, se nao matar na espadada mata no tetano."),
+        armadura("Armadura de Ferro", 100, 0, 60, "Essa daqui até que protege contra as falácias da I.A."),
+        pocao("Red Bull", 60, 30, 20, 20, 1))),
 
     asserta(loja(
-        espada(nome("Espada de Diamante"), preco(160), ataque(100), defesa(0), descricao("Espada do minecraft.")),
-        armadura(nome("Armadura de Diamante"), preco(160), ataque(0), defesa(100), descricao("Armadura do minecraft.")),
-        pocao(nome("Tyson Mentality"), preco(300), vida(0), ataque(100), defesa(0), quantidade(1)))).
+        espada("Espada de Diamante", 160, 100, 0, "Espada do minecraft."),
+        armadura("Armadura de Diamante", 160, 0, 100, "Armadura do minecraft."),
+        pocao("Tyson Mentality", 300, 0, 100, 0, 1))).
 
-exibe_loja :-
-    loja((espada(nome(Nome), preco(Preco), ataque(Ataque), defesa(Defesa), descricao(Descricao))),
-        (armadura(nome(Nome2), preco(Preco2), ataque(Ataque2), defesa(Defesa2), descricao(Descricao2))),
-        (pocao(nome(Nome3), preco(Preco3),vida(Vida), ataque(Ataque3), defesa(Defesa3), quantidade(Quantidade)))),
-    formata_espada(Nome, Preco, Ataque, Defesa, Descricao),
-    formata_armadura(Nome2, Preco2, Ataque2, Defesa2, Descricao2),
-    formata_pocao(Nome3, Preco3, Vida, Ataque3, Defesa3, Quantidade).
+printa_loja :-
+    loja((espada(Nome, Preco, Ataque, Defesa, Descricao)),
+        (armadura(Nome2, Preco2, Ataque2, Defesa2, Descricao2)),
+        (pocao(Nome3, Preco3, Vida, Ataque3, Defesa3, Quantidade))),
+    formata_espada(Nome, Preco, Ataque, Defesa, Descricao, String_espada),
+    formata_armadura(Nome2, Preco2, Ataque2, Defesa2, Descricao2, String_armadura),
+    formata_pocao(Nome3, Preco3, Vida, Ataque3, Defesa3, Quantidade, String_Pocao),
+    write(String_espada), write(String_armadura), write(String_Pocao).
 
-formata_espada(Nome, Preco, Ataque, Defesa, Descricao) :-
-    format(atom(String), " ~w | Preço: ~d \n Ataque: ~d | Defesa: ~d \n Descrição: ~w\n", [Nome, Preco, Ataque, Defesa, Descricao]),
-    writeln(String).
+formata_espada(Nome, Preco, Ataque, Defesa, Descricao, String) :-
+    format(atom(String), " ~w | Preço: ~d \n Ataque: ~d | Defesa: ~d \n Descrição: ~w\n\n", [Nome, Preco, Ataque, Defesa, Descricao]).
 
-formata_armadura(Nome, Preco, Ataque, Defesa, Descricao) :-
-    format(atom(String), " ~w | Preço: ~d \n Ataque: ~d | Defesa: ~d \n Descrição: ~w\n", [Nome, Preco, Ataque, Defesa, Descricao]),
-    writeln(String).
+formata_armadura(Nome, Preco, Ataque, Defesa, Descricao, String) :-
+    format(atom(String), " ~w | Preço: ~d \n Ataque: ~d | Defesa: ~d \n Descrição: ~w\n\n", [Nome, Preco, Ataque, Defesa, Descricao]).
 
-formata_pocao(Nome, Preco, Vida, Ataque, Defesa, Quantidade) :-
-    format(atom(String), " ~w | Preço: ~d \n Vida: ~d | Ataque: ~d | Defesa: ~d \n Quantidade: ~d", [Nome, Preco, Vida, Ataque, Defesa, Quantidade]),
-    writeln(String).
+formata_pocao(Nome, Preco, Vida, Ataque, Defesa, Quantidade, String) :-
+    format(atom(String), " ~w | Preço: ~d \n Vida: ~d | Ataque: ~d | Defesa: ~d \n Quantidade: ~d", [Nome, Preco, Vida, Ataque, Defesa, Quantidade]).
 
